79d473d439c5d6314253b2cad3373d744c995c16
Sunnyy09 <12001109@csepup.ac.in>
2024-12-08 10:45:52 +0530

    add backend services code using appwrite

---
+++ src/appwrite/auth.js
@@ -0,0 +1,67 @@
+import config from "../conf/config";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(config.appwriteUrl)
+      .setEndpoint(config.appwriteProjectId);
+
+    this.account = new Account(this.client);
+  }
+
+  async createAccount({ email, username, password }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        username
+      );
+      if (userAccount) {
+        return this.login({ email, password });
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      const session = await this.account.createEmailPasswordSession(
+        email,
+        password
+      );
+      return session;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      // console.log("Appwrite Service :: getCurrentUser :: error", error);
+      throw error;
+    }
+    return null;
+  }
+
+  async logout() {
+    try {
+      return await this.account.deleteSessions();
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+const authService = new AuthService();
+
+export default authService;
