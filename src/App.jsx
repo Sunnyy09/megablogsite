a607c481c209127f2d76a20f22dd24a8c2e74c3c
Sunnyy09 <12001109@csepup.ac.in>
2024-12-09 09:19:18 +0530

    add some changes and make components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,42 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
 import "./App.css";
+import authService from "./appwrite/auth";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import { login, logout } from "./features/authSlice/authSlice";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  return (
-    <>
-      <h1 className="text-center text-3xl underline">
-        mega blog project using react
-      </h1>
-    </>
-  );
+  const [loading, setLoading] = useState(true);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          TODO:
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
---
+++ src/components/Header/LogoutBtn.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import authService from "../../appwrite/auth";
+import logout from "../../features/authSlice/authSlice";
+
+function LogoutBtn() {
+  const dispatch = useDispatch();
+  const logouthandler = () => {
+    authService.logout().then(() => {
+      dispatch(logout());
+    });
+  };
+
+  return (
+    <button className="inline-block bg-blue-100 rounded-full">Logout</button>
+  );
+}
+
+export default LogoutBtn;
