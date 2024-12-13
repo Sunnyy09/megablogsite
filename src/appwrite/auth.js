4d2cb75e23be5dfbb940a250ed1cba3a653c25be
Sunnyy09 <12001109@csepup.ac.in>
2024-12-13 12:47:02 +0530

    add making of RTE.jsx and post form component using tinymce service

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -8,7 +8,7 @@ export class AuthService {
   constructor() {
     this.client
       .setEndpoint(config.appwriteUrl)
-      .setEndpoint(config.appwriteProjectId);
+      .setProject(config.appwriteProjectId);
 
     this.account = new Account(this.client);
   }
